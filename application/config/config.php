f17892864cbff12328a3f5ace201637e8d357428
ilker özcan <iletisim@ilkerozcan.com.tr>
2013-03-01 12:56:38 +0200

    config change

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.teslafilms.com';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
